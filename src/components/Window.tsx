1822ca5112d33e335f25985cc8ad3e41b7aa250a
Reda Elmountassir <redaelmountassir0@gmail.com>
2023-08-28 23:23:12 -0400

    Ton of changes, 3d, mobile

--- src/components/Window.tsx
+++ src/components/Window.tsx
@@ -1,8 +1,9 @@
 import { Point, motion, useDragControls, useMotionValue } from 'framer-motion';
 import * as React from 'react';
 import Resizers from './Resizers';
-import { useRef, useState, useEffect } from 'react';
+import { useRef, useState, useEffect, useContext } from 'react';
 import WindowHeader from './WindowHeader';
+import { MobileContext } from './OS';
 
 interface Dimensions {
 	w: number;
@@ -10,8 +11,9 @@ interface Dimensions {
 }
 
 interface WindowProps {
-	area: React.RefObject<Element>;
-	bringToFrontReq: React.PointerEventHandler;
+	area?: React.RefObject<Element>;
+	bringToFrontReq?: React.PointerEventHandler;
+	deleteReq?: Function;
 	initialLocation?: Point;
 	initialDimensions?: Dimensions;
 	minDimensions?: Dimensions;
@@ -20,20 +22,23 @@ interface WindowProps {
 const Window: React.FC<WindowProps> = ({
 	area,
 	bringToFrontReq,
+	deleteReq,
 	initialLocation = { x: 0, y: 0 },
 	initialDimensions = { w: 500, h: 300 },
 	minDimensions = { w: 200, h: 100 },
 }) => {
 	const [isMoving, setIsMoving] = useState(false);
+	const [maximized, setMaximized] = useState(false);
 	const width = useMotionValue(initialDimensions.w);
 	const height = useMotionValue(initialDimensions.h);
 	const x = useMotionValue(initialLocation.x);
 	const y = useMotionValue(initialLocation.y);
 	const controls = useDragControls();
 	const windowRef = useRef<HTMLDivElement>(null);
+	const isMobile = useContext(MobileContext);
 
 	return (
-		<motion.div
+		<motion.section
 			drag
 			dragListener={false}
 			dragControls={controls}
@@ -50,10 +55,15 @@ const Window: React.FC<WindowProps> = ({
 				document.documentElement.classList.remove('cursor-grab');
 				document.body.classList.remove('pointer-events-none');
 			}}
+			initial={isMobile ? {} : { scale: 0 }}
+			animate={isMobile ? {} : { scale: 1 }}
+			exit={isMobile ? {} : { scale: 0 }}
 			onPointerDown={bringToFrontReq}
 			ref={windowRef}
-			className={`absolute outline outline-2 outline-white-primary flex flex-col max-w-full max-h-full top-0 ${
-				isMoving && 'invisible'
+			className={`${
+				(isMobile || maximized) && '!w-full !h-full !transform-none'
+			} pointer-events-auto absolute outline backdrop-blur bg-black-primary from-black-primary/75 to-dark-primary/75 from-25% to-70% outline-2 outline-white-primary flex flex-col max-w-full max-h-full top-0 shadow-[10px_10px_0_0] shadow-black-primary/25 md:bg-gradient-to-r md:bg-transparent ${
+				isMoving && 'invisible backdrop-blur-none'
 			}`}
 			style={{
 				minWidth: minDimensions.w,
@@ -64,45 +74,76 @@ const Window: React.FC<WindowProps> = ({
 				height,
 			}}
 		>
-			<WindowHeader onGrab={e => controls.start(e)} />
-			<div className='grow bg-black'></div>
-			<Resizers
-				onResizeStart={() => {
-					setIsMoving(true);
-				}}
-				onResizeEnd={() => {
-					setIsMoving(false);
-				}}
-				onResize={(info, cardinal) => {
-					switch (cardinal) {
-						case 'nw':
-							x.set(x.get() + info.delta.x);
-							width.set(width.get() - info.delta.x);
-						case 'n':
-							y.set(y.get() + info.delta.y);
-							height.set(height.get() - info.delta.y);
-							break;
-						case 'ne':
-							y.set(y.get() + info.delta.y);
-							height.set(height.get() - info.delta.y);
-						case 'e':
-							width.set(width.get() + info.delta.x);
-							break;
-						case 'se':
-							width.set(width.get() + info.delta.x);
-						case 's':
-							height.set(height.get() + info.delta.y);
-							break;
-						case 'sw':
-							height.set(height.get() + info.delta.y);
-						case 'w':
-							x.set(x.get() + info.delta.x);
-							width.set(width.get() - info.delta.x);
-							break;
-					}
-				}}
-			/>
-		</motion.div>
+			{!isMobile && (
+				<WindowHeader
+					onGrab={e => controls.start(e)}
+					onClose={() => deleteReq && deleteReq()}
+					onMaximize={() => setMaximized(maximized => !maximized)}
+					maximized={maximized}
+				/>
+			)}
+			<div className='grow overflow-y-auto'></div>
+			{!isMobile && (
+				<Resizers
+					onResizeStart={() => setIsMoving(true)}
+					onResizeEnd={() => setIsMoving(false)}
+					onResize={({ delta }, cardinal) => {
+						const westResize = () => {
+							//Prevents negative dragging
+							const w = width.get();
+							let newWidth = w - delta.x;
+							let newX = delta.x;
+							if (newWidth < minDimensions.w) {
+								newWidth = minDimensions.w;
+								newX = w - newWidth;
+							}
+
+							x.set(x.get() + newX);
+							width.set(newWidth);
+						};
+
+						const northResize = () => {
+							//Prevents negative dragging
+							const h = height.get();
+							let newHeight = h - delta.y;
+							let newY = delta.y;
+							if (newHeight < minDimensions.h) {
+								newHeight = minDimensions.h;
+								newY = h - newHeight;
+							}
+
+							y.set(y.get() + newY);
+							height.set(newHeight);
+						};
+
+						switch (cardinal) {
+							case 'nw':
+								westResize();
+							case 'n':
+								northResize();
+								break;
+							case 'ne':
+								y.set(y.get() + delta.y);
+								height.set(height.get() - delta.y);
+							case 'e':
+								width.set(width.get() + delta.x);
+								break;
+							case 'se':
+								width.set(width.get() + delta.x);
+							case 's':
+								height.set(height.get() + delta.y);
+								break;
+							case 'sw':
+								height.set(height.get() + delta.y);
+							case 'w':
+								westResize();
+								break;
+						}
+					}}
+					showOutline={isMoving}
+				/>
+			)}
+		</motion.section>
 	);
 };
 

7f9733edb989f7d042b4f88c1b2a38989cfa0ea6
Reda Elmountassir <redaelmountassir0@gmail.com>
2024-10-24 16:50:00 -0400

    bug fixes ig

--- src/components/SEO.tsx
+++ src/components/SEO.tsx
@@ -2,42 +2,41 @@ import React, { memo } from "react";
 import { useSiteMetadata } from "../utils";
 
 interface SEOProps {
-  description?: "string";
-  pathname?: "string";
-  children?: React.ReactNode;
+	description?: "string";
+	pathname?: "string";
+	children?: React.ReactNode;
 }
 
 export const SEO: React.FC<SEOProps> = memo(
-  ({ description, pathname, children }) => {
-    const {
-      title,
-      description: defaultDescription,
-      image,
-      siteUrl,
-      twitterUsername,
-    } = useSiteMetadata();
+	({ description, pathname, children }) => {
+		const {
+			description: defaultDescription,
+			image,
+			siteUrl,
+			twitterUsername,
+		} = useSiteMetadata();
 
-    const seo = {
-      title: title || "RedaOS",
-      description: description || defaultDescription,
-      image: `${siteUrl}${image}`,
-      url: `${siteUrl}${pathname || ``}`,
-      twitterUsername,
-    };
+		const seo = {
+			title: "RedaOS",
+			description: description || defaultDescription,
+			image: `${siteUrl}${image}`,
+			url: `${siteUrl}${pathname || ``}`,
+			twitterUsername,
+		};
 
-    return (
-      <>
-        <title id="title">{seo.title}</title>
-        <meta name="description" content={seo.description} />
-        <meta name="image" content={seo.image} />
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:title" content={seo.title} />
-        <meta name="twitter:url" content={seo.url} />
-        <meta name="twitter:description" content={seo.description} />
-        <meta name="twitter:image" content={seo.image} />
-        <meta name="twitter:creator" content={seo.twitterUsername} />
-        {children}
-      </>
-    );
-  },
+		return (
+			<>
+				<title id="title">{seo.title}</title>
+				<meta name="description" content={seo.description} />
+				<meta name="image" content={seo.image} />
+				<meta name="twitter:card" content="summary_large_image" />
+				<meta name="twitter:title" content={seo.title} />
+				<meta name="twitter:url" content={seo.url} />
+				<meta name="twitter:description" content={seo.description} />
+				<meta name="twitter:image" content={seo.image} />
+				<meta name="twitter:creator" content={seo.twitterUsername} />
+				{children}
+			</>
+		);
+	},
 );
